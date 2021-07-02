7637a1a7a486be2a4f5e179f787f44f35158b316
Diimsch <57183037+Diimsch@users.noreply.github.com>
2021-07-02 17:00:52 +0200

    better shake

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -561,7 +561,7 @@ public class Player : MonoBehaviour
         _rb.velocity = Vector2.zero;
         dust.Play();
         SoundManagerScript.PlaySound("Dash");
-        CameraShake.Instance.ShakeCamera(3f, 0.1f);
+        CameraShake.Instance.ShakeCamera(2f, 0.2f);
 
 
         float disableGravityForSecs = 0.05f;

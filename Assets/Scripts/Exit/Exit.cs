3ee5027c45716f06a2889acbba6ed72892900f9f
Diimsch <57183037+Diimsch@users.noreply.github.com>
2021-07-14 14:01:52 +0200

    fix level 1 consitent level end

--- Assets/Scripts/Exit/Exit.cs
+++ Assets/Scripts/Exit/Exit.cs
@@ -17,17 +17,6 @@ public class Exit : MonoBehaviour
     
     public void OnExit()
     {
-        switch (exitPath)
-        {
-            case ExitPath.MainMenu:
-                SceneManager.LoadScene("Scenes/StartMenu");
-                break;
-            case ExitPath.Tutorial:
-                SceneManager.LoadScene("Scenes/Tutorial");
-                break;
-            case ExitPath.Level1:
-                SceneManager.LoadScene("Scenes/Level 1");
-                break;
-        }
+        SceneManager.LoadScene("Scenes/StartMenu");
     }
 }
